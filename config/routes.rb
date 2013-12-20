1db8860c695959892495ab689c808c5de8fc5023
Manuel Meurer <manuel@krautcomputing.com>
2013-12-20 18:48:07 +0100

    Add Sidekiq stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require 'sidekiq/web'
+
 RubyDocs::Application.routes.draw do
   resources :doc_collections, only: :create
   get ':doc_collection_slug' => 'doc_collections#show', as: 'doc_collection'
-  
+
   root to: 'pages#show', id: 'home'
 end
