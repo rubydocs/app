1b783c8948cf06662d13d1ac073f25027db80870
Manuel Meurer <manuel@krautcomputing.com>
2013-12-20 19:04:21 +0100

    Add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 require 'sidekiq/web'
 
 RubyDocs::Application.routes.draw do
+  get 'favicon.ico' => redirect(ActionController::Base.helpers.image_path('favicon.ico'))
+
   resources :doc_collections, only: :create
   get ':doc_collection_slug' => 'doc_collections#show', as: 'doc_collection'
 
