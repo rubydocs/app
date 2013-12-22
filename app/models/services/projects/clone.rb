2ffc5c1d83ecdfa77a088349e55a44904da5a499
Manuel Meurer <manuel@krautcomputing.com>
2013-12-22 08:29:17 +0100

    Formatting, code style

--- app/models/services/projects/clone.rb
+++ app/models/services/projects/clone.rb
@@ -6,8 +6,7 @@ module Services
       FolderExistsError = Class.new(Error)
 
       def call(project)
-        raise FolderExistsError, "Folder #{project.local_path} already exists and is not empty." if File.exist?(project.local_path) && !Dir[File.join(project.local_path, '*')].empty?
-
+        raise FolderExistsError, "Folder #{project.local_path} already exists and is not empty." if File.exist?(project.local_path) && Dir[File.join(project.local_path, '*')].present?
         dir = File.expand_path('..', project.local_path)
         Git.clone project.git, project.slug, path: dir
         project
