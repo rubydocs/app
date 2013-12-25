f30cf839637d4d4091fba176960bfd4b7eda6518
Manuel Meurer <manuel@krautcomputing.com>
2013-12-25 14:08:48 +0100

    Refactor doc collection services

--- app/models/services/doc_collections/upload_files.rb
+++ app/models/services/doc_collections/upload_files.rb
@@ -1,17 +1,45 @@
 module Services
   module DocCollections
     class UploadFiles < Services::Base
-      def call(doc_collection)
+      BATCH_SIZE = 100
+
+      def call(doc_collection_id)
+        doc_collection = DocCollection.where(id: doc_collection_id).first
+        raise Error, "Doc collection with ID #{doc_collection_id} not found." if doc_collection.nil?
+
         s3 = AWS::S3.new
         bucket = s3.buckets[Settings.aws.bucket]
+
+        # Check which files are already uploaded
         dir = File.expand_path('..', doc_collection.local_path)
-        Dir.chdir dir do
-          Dir[File.join(doc_collection.slug, '**', '*')].each do |path|
-            next unless File.file?(path)
-            content_type = MimeMagic.by_path(path)
-            bucket.objects[path].write(file: path, content_type: content_type)
-          end
+        all_files = Dir.chdir(dir) do
+          Dir[File.join(doc_collection.slug, '**', '*')]
         end
+        uploaded_files = bucket.objects.with_prefix(doc_collection.slug).map(&:key)
+        remaining_files = all_files - uploaded_files
+
+        # Upload remaining files
+        remaining_files.take(BATCH_SIZE).each do |file|
+          if File.file?(file)
+            content_type = MimeMagic.by_path(file)
+            bucket.objects[file].write(file: file, content_type: content_type)
+          end
+          remaining_files.delete(file)
+        end
+
+        # Enqueue yourself if there are more files to upload
+        if remaining_files.present?
+          self.class.perform_async :call, doc_collection.id
+          return
+        end
+
+        # Set uploaded_at timestamp
+        doc_collection.uploaded_at = Time.now
+        doc_collection.save!
+
+        # Delete doc collection files
+        Services::DocCollections::DeleteFiles.call doc_collection
+
         doc_collection
       end
     end
--- app/models/services/process_doc_collection.rb
+++
@@ -1,8 +0,0 @@
-module Services
-  class ProcessDocCollection < Services::Base
-    def call(doc_collection_id)
-      Services::DocCollections::GenerateDocCollection.call doc_collection_id
-      Services::DocCollections::UploadDocCollection.perform_async :call, doc_collection_id
-    end
-  end
-end
