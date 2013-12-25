a2b0639510133a8e7079383c420aaa38ff2dd6f0
Manuel Meurer <manuel@krautcomputing.com>
2013-12-25 11:59:02 +0100

    Improve mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,11 +1,13 @@
 class Mailer < ActionMailer::Base
-  default from: Settings.mail.from
+  FRIENDLY_FROM = "Ruby Docs <#{Settings.mail.from}>"
+
+  default from: FRIENDLY_FROM
 
   def doc_collection_generated(doc_collection, emails)
     @doc_collection = doc_collection
 
     mail \
-      to:      Settings.mail.from,
+      to:      FRIENDLY_FROM,
       bcc:     emails,
       subject: "[Ruby Docs] #{@doc_collection.name} docs are ready!"
   end
