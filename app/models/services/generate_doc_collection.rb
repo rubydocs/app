d9ff6e2f5dc147c1e893bf9fd0ef58ab703414c9
Manuel Meurer <manuel@krautcomputing.com>
2013-12-25 12:52:25 +0100

    Split up processing doc collections

---
+++ app/models/services/generate_doc_collection.rb
@@ -0,0 +1,34 @@
+module Services
+  class GenerateDocCollection < Services::Base
+    def call(doc_collection_id)
+      doc_collection = DocCollection.where(id: doc_collection_id).first
+      raise Error, "Doc collection with ID #{doc_collection_id} not found." if doc_collection.nil?
+
+      # Create files for docs
+      doc_collection.docs.each do |doc|
+        begin
+          Services::Docs::CreateFiles.call doc
+        rescue Services::Docs::CreateFiles::CreatingInProgressError
+          self.class.perform_in 1.minute, doc_collection_id
+          return
+        rescue Services::Docs::CreateFiles::FilesExistsError
+          next
+        end
+      end
+
+      # Create doc collection files
+      Services::DocCollections::CreateFiles.call doc_collection
+
+      # Set generated_at timestamp
+      doc_collection.generated_at = Time.now
+      doc_collection.save!
+
+      # Send notifications
+      emails = EmailNotification.by_doc_collection(doc_collection).map(&:email)
+      Mailer.doc_collection_generated(doc_collection, emails).deliver! if emails.present?
+      # TODO: Delete email notifications
+
+      doc_collection
+    end
+  end
+end
--- app/models/services/process_doc_collection.rb
+++ app/models/services/process_doc_collection.rb
@@ -1,46 +1,8 @@
 module Services
   class ProcessDocCollection < Services::Base
     def call(doc_collection_id)
-      doc_collection = DocCollection.where(id: doc_collection_id).first
-      raise Error, "Doc collection with ID #{doc_collection_id} not found." if doc_collection.nil?
-
-      # Create files for docs
-      doc_collection.docs.each do |doc|
-        begin
-          Services::Docs::CreateFiles.call doc
-        rescue Services::Docs::CreateFiles::CreatingInProgressError
-          self.class.perform_in 1.minute, doc_collection_id
-          return
-        rescue Services::Docs::CreateFiles::FilesExistsError
-          next
-        end
-      end
-
-      # Create doc collection files
-      Services::DocCollections::CreateFiles.call doc_collection
-
-      # Set generated_at timestamp
-      doc_collection.generated_at = Time.now
-      doc_collection.save!
-
-      # Send notifications
-      emails = EmailNotification.by_doc_collection(doc_collection).map(&:email)
-      Mailer.doc_collection_generated(doc_collection, emails).deliver! if emails.present?
-      # TODO: Delete email notifications
-
-      unless Rails.env.development?
-        # Upload doc collection files to cloud
-        Services::DocCollections::UploadFiles.call doc_collection
-
-        # Set uploaded_at timestamp
-        doc_collection.uploaded_at = Time.now
-        doc_collection.save!
-
-        # Delete doc collection files
-        Services::DocCollections::DeleteFiles.call doc_collection
-      end
-
-      doc_collection
+      Services::DocCollections::GenerateDocCollection.call doc_collection_id
+      Services::DocCollections::UploadDocCollection.perform_async :call, doc_collection_id unless Rails.env.development?
     end
   end
 end
