2e810e14d08118980c768903931ed57f4ff70684
Manuel Meurer <manuel@krautcomputing.com>
2013-12-30 13:59:04 +0100

    Store puma.sock file in shared folder

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,4 +3,4 @@ pidfile 'tmp/pids/puma.pid'
 state_path 'tmp/pids/puma.state'
 stdout_redirect 'log/puma.log', 'log/puma_err.log', true
 threads 0, 16
-bind 'unix://tmp/puma.sock'
+bind 'unix://tmp/pids/puma.sock'
