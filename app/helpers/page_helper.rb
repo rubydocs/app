1489319a8c64dd00ec483077ceab3d7fc23960e1
Manuel Meurer <manuel@krautcomputing.com>
2013-12-30 19:48:16 +0100

    Fix versions

--- app/helpers/page_helper.rb
+++ app/helpers/page_helper.rb
@@ -2,6 +2,6 @@ module PageHelper
   def project_versions(project)
     versions = Services::Projects::ConvertTagsToVersions.call(project.tags)
     versions.reject! { |k, v| v.nil? }
-    Hash[*versions.sort_by(&:last).reverse.flatten]
+    Hash[*versions.sort_by(&:last).reverse.flatten].invert
   end
 end
