c66107a0fb23ad713dd3a4daec0b63bf3312919b
Manuel Meurer <manuel@krautcomputing.com>
2013-12-30 23:10:37 +0100

    Update comment

--- app/models/activity_log.rb
+++ app/models/activity_log.rb
@@ -2,8 +2,7 @@ class ActivityLog
   def initialize
     log_file = Rails.root.join('log', 'activity.log')
     @logger = ActiveSupport::TaggedLogging.new(Logger.new(log_file))
-    # TODO: Does this actually work?
-    # Remove existing tags (`Rails.application.config.log_tags`)
+    # Remove existing tags (Rails.application.config.log_tags)
     @logger.clear_tags!
   end
 
