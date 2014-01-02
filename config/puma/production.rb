468c26b85456c55e107f815e07f0946fef0e1ab8
Manuel Meurer <manuel@krautcomputing.com>
2014-01-02 12:36:58 +0100

    Update Puma config

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -2,5 +2,6 @@ environment 'production'
 pidfile 'tmp/pids/puma.pid'
 state_path 'tmp/pids/puma.state'
 stdout_redirect 'log/puma.log', 'log/puma_err.log', true
-threads 0, 16
+threads 0, 4
+workers 1
 bind 'unix://tmp/pids/puma.sock'
