6371b48d5cff53d72899feb9ae8332485f5c9138
Manuel Meurer <manuel@krautcomputing.com>
2014-01-14 12:07:45 +0100

    Small improvement in asyncable concern

--- app/models/concerns/asyncable.rb
+++ app/models/concerns/asyncable.rb
@@ -16,15 +16,13 @@ module Asyncable
   alias_method :perform_at, :perform_in
 
   def perform(*args)
+    return self.call(*args) if self.is_a?(Services::Base)
+
     target = if args.last.is_a?(Hash) && args.last.keys.first.to_sym == TARGET_PARAM_NAME
       self.class.find args.pop.values.first
     else
       self.class
     end
-
-    # Add :call as first argument if not present and target is a service
-    # Sidetiq simply calls `perform` without arguments
-    args = args.dup.unshift(:call) if target.is_a?(Class) && target < Services::Base && args.first.to_s != 'call'
 
     target.send *args
   end
