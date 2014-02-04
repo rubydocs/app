fba26100841177f29c1b5dac660b7f0554fe8d8e
Manuel Meurer <manuel@krautcomputing.com>
2014-02-04 12:10:36 +0100

    Formatting

--- app/models/services/doc_collections/create_files.rb
+++ app/models/services/doc_collections/create_files.rb
@@ -9,6 +9,7 @@ module Services
         docs = Services::Docs::Find.call([], doc_collection: doc_collection)
         raise Error, "Doc collection #{doc_collection.name} has no docs." if docs.empty?
         raise FolderExistsError, "Folder for doc collection #{doc_collection.name} already exist." if File.exist?(doc_collection.local_path)
+
         sdoc_merge = SDoc::Merge.new
         args = [
           "--title=#{doc_collection.name}",
@@ -17,6 +18,7 @@ module Services
           *docs.map(&:local_path)
         ]
         sdoc_merge.merge args
+
         doc_collection
       end
     end
