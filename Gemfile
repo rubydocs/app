cd38e9a910981ac164102ec97f05de88b40bd809
Manuel Meurer <manuel@krautcomputing.com>
2014-03-02 13:37:21 +0100

    Use s3cmd for syncing files

--- Gemfile
+++ Gemfile
@@ -23,7 +23,6 @@ gem 'friendly_id',                    '5.0.2'
 gem 'squeel',                         '1.1.1'
 gem 'capistrano',                     '2.15.5'
 gem 'postgres_ext',                   '2.1.3'
-gem 'mimemagic',                      '0.2.1'
 gem 'airbrake',                       '3.1.14'
 
 # Frontend
--- app/models/services/doc_collections/upload_files.rb
+++ app/models/services/doc_collections/upload_files.rb
@@ -1,36 +1,11 @@
 module Services
   module DocCollections
     class UploadFiles < Services::Base
-      BATCH_SIZE = 100
-
       def call(doc_collection_id)
         doc_collection = Services::DocCollections::Find.call(doc_collection_id).first
         raise Error, "Doc collection #{doc_collection_id} not found." if doc_collection.nil?
 
-        s3 = AWS::S3.new
-        bucket = s3.buckets[Settings.aws.bucket]
-
-        # Check which files are already uploaded
-        dir = File.expand_path('..', doc_collection.local_path)
-        Dir.chdir dir
-        all_files = Dir[File.join(doc_collection.slug, '**', '*')].select do |file|
-          File.file?(file)
-        end
-        uploaded_files = bucket.objects.with_prefix(doc_collection.slug).map(&:key)
-        remaining_files = all_files - uploaded_files
-
-        # Upload remaining files
-        remaining_files.take(BATCH_SIZE).each do |file|
-          content_type = MimeMagic.by_path(file).to_s
-          bucket.objects[file].write(file: file, content_type: content_type)
-          remaining_files.delete(file)
-        end
-
-        # Enqueue yourself if there are more files to upload
-        if remaining_files.present?
-          self.class.perform_async doc_collection.id
-          return
-        end
+        Services::SyncFiles.call doc_collection.local_path
 
         # Set uploaded_at timestamp
         doc_collection.uploaded_at = Time.now
