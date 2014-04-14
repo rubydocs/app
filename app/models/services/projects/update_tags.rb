b61ca7c13c789817d0bfda650a7e225b539ad560
Manuel Meurer <manuel@krautcomputing.com>
2014-04-14 21:45:19 +0200

    Retry updating git tags on git error

--- app/models/services/projects/update_tags.rb
+++ app/models/services/projects/update_tags.rb
@@ -7,7 +7,9 @@ module Services
         project = Services::Projects::Find.call(project_id).first
         raise Error, "Could not find project #{project_id}" if project.nil?
         git = Git.open(project.local_path)
-        git.fetch 'origin', tags: true
+        3.tries on: Git::GitExecuteError, delay: 1 do
+          git.fetch 'origin', tags: true
+        end
         tags = git.tags.each_with_object({}) do |tag, hash|
           hash[tag.name] = git.gcommit(tag.sha).date
         end
