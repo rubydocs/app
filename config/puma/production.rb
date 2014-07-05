bdfb6cf8fe27834d2d83287f768e8d0d22315262
Manuel Meurer <manuel@krautcomputing.com>
2014-07-05 21:29:00 +0200

    Remove Puma config

--- config/puma/production.rb
+++
@@ -1,7 +0,0 @@
-environment 'production'
-pidfile 'tmp/pids/puma.pid'
-state_path 'tmp/pids/puma.state'
-stdout_redirect 'log/puma.log', 'log/puma_err.log', true
-threads 0, 4
-workers 1
-bind 'unix://tmp/pids/puma.sock'
