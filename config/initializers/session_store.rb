b721ffbc0758529669a6611d3c2a25f2285adfa5
Manuel Meurer <manuel@krautcomputing.com>
2014-07-08 18:08:59 +0200

    Small code fix

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
-RubyDocs::Application.config.session_store :redis_store,
+Rails.application.config.session_store :redis_store,
   redis_server: REDIS_CONFIG.merge(namespace: 'sessions'),
   expires_in: 1.day
