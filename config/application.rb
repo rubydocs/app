b88901b74f6b6e53d5b28d98aca1b02613d9ad3d
Manuel Meurer <manuel@krautcomputing.com>
2014-08-15 17:03:18 +0200

    Formatting

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'sprockets/railtie'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module RubyDocs
