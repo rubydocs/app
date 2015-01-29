e4d965afdb6751d5cbca60645adc147347d69754
Manuel Meurer <manuel@krautcomputing.com>
2015-01-29 08:52:12 +0100

    update mailer

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,12 +1,12 @@
 class Mailer < ActionMailer::Base
-  default \
-    from: "RubyDocs <#{Settings.mail.from}>",
-    to:   Settings.mail.to
-
   def doc_collection_generated(doc_collection, emails)
     @doc_collection = doc_collection
 
+    friendly_from = "RubyDocs <#{Settings.mail.from}>"
+
     mail \
+      from:    friendly_from,
+      to:      friendly_from,
       bcc:     emails,
       subject: "[RubyDocs] #{@doc_collection.name} docs are ready!"
   end
