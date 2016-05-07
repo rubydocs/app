a2c5f2615c2f31e0d6766db61d4d0ba8dadc7a28
Manuel Meurer <manuel@krautcomputing.com>
2016-05-07 09:28:52 +0200

    add missing service namespace

--- app/services/doc_collections/delete_files.rb
+++ app/services/doc_collections/delete_files.rb
@@ -1,9 +1,11 @@
-class DeleteFiles < Services::Base
-  def call(id_or_object)
-    doc_collection = find_object(id_or_object)
-    [doc_collection.local_path, doc_collection.zipfile].each do |path|
-      FileUtils.rm_rf path
+module DocCollections
+  class DeleteFiles < Services::Base
+    def call(id_or_object)
+      doc_collection = find_object(id_or_object)
+      [doc_collection.local_path, doc_collection.zipfile].each do |path|
+        FileUtils.rm_rf path
+      end
+      doc_collection
     end
-    doc_collection
   end
 end
