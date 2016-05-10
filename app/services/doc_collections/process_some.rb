8e513febffc02e6cf9802af1aa0e9a3411d95cb9
Manuel Meurer <manuel@krautcomputing.com>
2016-05-10 17:22:38 +0200

    generate doc collections in the order they were created

--- app/services/doc_collections/process_some.rb
+++ app/services/doc_collections/process_some.rb
@@ -3,7 +3,7 @@ module DocCollections
     def call
       check_uniqueness on_error: :return
 
-      doc_collection_to_generate = DocCollections::Find.call([], generated_at: nil).first || begin
+      doc_collection_to_generate = DocCollections::Find.call([], generated_at: nil, order: 'created_at').first || begin
         current_generator_version = DocGenerators::Sdoc::GetVersion.call
         DocCollections::Find.call([], generated_at_not: nil, generated_with_not: current_generator_version, order: 'generated_at').last
       end
