259b81b8ba50dc2967c33c24d2dbf6122f156c01
Manuel Meurer <manuel@krautcomputing.com>
2016-05-13 12:36:28 +0200

    comment

--- app/services/doc_collections/create.rb
+++ app/services/doc_collections/create.rb
@@ -3,7 +3,7 @@ module DocCollections
     def call(docs)
       doc_collection = DocCollection.new
       docs.each do |doc|
-        # Cannot use doc_collection.doc_collection_memberships.build() here, otherwise validations fail
+        # Cannot use doc_collection.doc_collection_memberships.build() here, otherwise validations fail.
         doc_collection.doc_collection_memberships << DocCollectionMembership.new(doc: doc, doc_collection: doc_collection)
       end
       doc_collection.save!
