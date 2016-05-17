dc209077d31ec5f7d2fe0f1a5a4039942ddad97a
Manuel Meurer <manuel@krautcomputing.com>
2016-05-18 01:54:24 +0200

    allow passing more attributes when creating project

--- app/services/projects/create.rb
+++ app/services/projects/create.rb
@@ -1,7 +1,7 @@
 module Projects
   class Create < Services::Base
-    def call(name, git)
-      project = Project.create!(name: name, git: git)
+    def call(attributes)
+      project = Project.create!(attributes)
       Projects::Clone.call(project)
       Projects::UpdateTags.call(project.id)
       # Reload project so that tags are visible.
