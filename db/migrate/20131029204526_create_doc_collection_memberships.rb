227bd1dd506ab29567f62fedac94a2b7bf8b7a48
Manuel Meurer <manuel@krautcomputing.com>
2017-05-11 17:01:01 +0200

    fix migration

--- db/migrate/20131029204526_create_doc_collection_memberships.rb
+++ db/migrate/20131029204526_create_doc_collection_memberships.rb
@@ -1,7 +1,8 @@
 class CreateDocCollectionMemberships < ActiveRecord::Migration
   def change
     create_table :doc_collection_memberships do |t|
-      t.references :doc, :doc_collection
+      t.references :doc
+      t.references :doc_collection
     end
 
     add_index :doc_collection_memberships, %i(doc_id doc_collection_id), unique: true, name: 'index_doc_collection_m_on_doc_id_and_doc_collection_id'
