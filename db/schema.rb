5089e4ca0fdb4b2b4bb52897549342bf8e06d50f
Manuel Meurer <manuel@krautcomputing.com>
2017-05-11 17:01:23 +0200

    update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,40 +16,36 @@ ActiveRecord::Schema.define(version: 20160513104329) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "doc_collection_memberships", force: true do |t|
-    t.integer "doc_id"
+  create_table "doc_collection_memberships", force: :cascade do |t|
+    t.integer "doc_id",            :index=>{:name=>"index_doc_collection_m_on_doc_id_and_doc_collection_id", :with=>["doc_collection_id"], :unique=>true, :using=>:btree}
     t.integer "doc_collection_id"
-    t.index ["doc_id", "doc_collection_id"], :name => "index_doc_collection_m_on_doc_id_and_doc_collection_id", :unique => true
   end
 
-  create_table "doc_collections", force: true do |t|
-    t.string   "slug",           null: false
+  create_table "doc_collections", force: :cascade do |t|
+    t.string   "slug",           :null=>false, :index=>{:name=>"index_doc_collections_on_slug", :unique=>true, :using=>:btree}
     t.datetime "generated_at"
     t.datetime "uploaded_at"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "generated_with"
-    t.index ["slug"], :name => "index_doc_collections_on_slug", :unique => true
   end
 
-  create_table "docs", force: true do |t|
+  create_table "docs", force: :cascade do |t|
     t.string   "tag"
-    t.string   "slug",       null: false
+    t.string   "slug",       :null=>false, :index=>{:name=>"index_docs_on_slug", :unique=>true, :using=>:btree}
     t.integer  "project_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.index ["slug"], :name => "index_docs_on_slug", :unique => true
   end
 
-  create_table "projects", force: true do |t|
+  create_table "projects", force: :cascade do |t|
     t.string   "name"
     t.string   "git"
-    t.string   "slug",                    null: false
+    t.string   "slug",       :null=>false, :index=>{:name=>"index_projects_on_slug", :unique=>true, :using=>:btree}
+    t.json     "tags"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.json     "tags"
-    t.text     "links",      default: [], null: false, array: true
-    t.index ["slug"], :name => "index_projects_on_slug", :unique => true
+    t.text     "links",      :default=>[], :null=>false, :array=>true
   end
 
 end
