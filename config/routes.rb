87306c1ac6aaae7c858b790d21f2aa58089621b7
Manuel Meurer <manuel@krautcomputing.com>
2018-09-02 20:31:59 +0200

    redirect sitemap requests to s3

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'favicon.ico' => redirect(ActionController::Base.helpers.image_path('favicon.ico'))
+  get 'favicon.ico'    => redirect(ActionController::Base.helpers.image_path('favicon.ico'))
+  get 'sitemap(*rest)' => redirect { |_, req| "https://s3.amazonaws.com/rubydocs#{req.path}" }
 
   resources :email_notifications, only: :create
   resources :doc_collections, only: :create
