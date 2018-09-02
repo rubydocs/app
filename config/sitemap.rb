dda4465eb42a88f2dc8777519677175f38cafff3
Manuel Meurer <manuel@krautcomputing.com>
2018-09-02 18:07:07 +0200

    update sitemap config

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,6 +1,7 @@
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(Settings.aws.bucket)
 SitemapGenerator::Sitemap.default_host = "https://#{Settings.host}"
 SitemapGenerator::Sitemap.create do
-  DocCollections::Find.call(uploaded_at: true).find_each do |doc_collection|
+  DocCollections::Find.call(uploaded_at: true, order: 'uploaded_at DESC').find_each do |doc_collection|
     add "/d/#{doc_collection.slug}/", lastmod: doc_collection.uploaded_at, priority: 0.75
     doc_collection.file_paths.each do |file_path|
       add "/d/#{doc_collection.slug}/#{file_path}", lastmod: doc_collection.uploaded_at
