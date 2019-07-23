77961a27ff6937ffe6d7229f3bda82f86da7d470
Manuel Meurer <manuel@krautcomputing.com>
2019-07-23 10:38:13 +0200

    create parent folder if it doesnt exist yet

--- app/services/docs/create_git_files.rb
+++ app/services/docs/create_git_files.rb
@@ -6,6 +6,8 @@ module Docs
       git.checkout doc.tag
       if File.exist?(doc.local_git_path)
         FileUtils.rm_rf doc.local_git_path
+      else
+        FileUtils.mkdir_p doc.local_git_path.dirname
       end
       FileUtils.cp_r doc.project.local_path, doc.local_git_path
     end
