171ea5edcd8ca5374deb809690f86f61caeba8d9
Manuel Meurer <manuel@krautcomputing.com>
2019-07-31 17:57:41 +0200

    silence warnings while loading rails gems

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,16 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_mailer/railtie'
-require 'sprockets/railtie'
+# Silence warnings while loading Rails gems, since we're using Ruby 2.6 and Rails 4,
+# we would otherwise get "warning: BigDecimal.new is deprecated; use BigDecimal() method instead."
+old_verbose, $VERBOSE = $VERBOSE, nil
+begin
+  require 'active_record/railtie'
+  require 'action_controller/railtie'
+  require 'action_mailer/railtie'
+  require 'sprockets/railtie'
+ensure
+  $VERBOSE = old_verbose
+end
 
 I18n.config.enforce_available_locales = false
 
