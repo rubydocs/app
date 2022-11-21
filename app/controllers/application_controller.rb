b95aa894a854bbe077a23b3cd858f241212e80a3
Manuel Meurer <manuel@meurer.io>
2022-11-21 12:03:42 +0100

    add cloudflare 2fa verify stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   def home
     render "shared/home"
   end
+
+  def cloudflare_verify
+    render plain: ENV.fetch("CLOUDFLARE_2FA_VERIFY")
+  end
 end
