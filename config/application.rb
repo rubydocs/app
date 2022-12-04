7ed671acc21314c47871987dd88e34841e41710e
Manuel Meurer <manuel@meurer.io>
2022-12-04 08:28:50 +0100

    determine revision

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module RailsBump
   class Application < Rails::Application
     config.load_defaults 7.0
-    config.revision = `git rev-parse --short HEAD 2> /dev/null`.chomp
+    config.revision = `git rev-parse --short HEAD 2> /dev/null`.chomp.presence ||
+                      ENV["FLY_IMAGE_REF"]&.then { _1[/@sha256:([0-9a-f]+)/, 1] } or
+                      raise "Could not determine revision."
+
     config.action_mailer.delivery_method = :postmark
     config.active_record.query_log_tags_enabled = true
 
